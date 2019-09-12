495c58a1c5dc3b28188e921c5059c3ba44825e5b
Anna Lunde <Anna@wifi-172-23-189-247.uoa-wifi.auckland.ac.nz>
2019-09-12 18:38:38 +1200

    part1a first exercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+  
+    return (
+      <div>
+         <Header course={course} />
+        
+         <Content partname={part1} exercisesnum={exercises1} />
+        
+        <Content partname={part2} exercisesnum={exercises2} />
+        
+        <Content partname={part3} exercisesnum={exercises3} />
+        
+        <Total totalnum={exercises1 + exercises2 + exercises3}/>
+      </div>
+    )
+  }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  const  Header = (props) => {
+    return (
+        <div>
+          <h1>
+            {props.course}
+          </h1>
+        </div>
+      )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  }
+
+  const Content = (props) => {
+    return (
+        <div>
+          <p>
+            {props.partname} has {props.exercisesnum} exercises
+          </p>
+        </div>
+      )
+      
+}
+
+  const Total = (props) => {
+    return (
+        <div>
+          <p>
+            The total number of exercises is {props.totalnum} 
+          </p>
+        </div>
+      )
+      
+}
+  
+  ReactDOM.render(<App />, document.getElementById('root'))
+  
+
+  /*
+  const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}*/
